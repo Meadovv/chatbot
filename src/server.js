86e82056194b4d6fcad3e5d2599ea018be213b1d
meadovv <hoangthanh2k3@gmail.com>
2023-08-11 22:54:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,18 @@ import bodyParser from "body-parser";
 import viewEngine from "./configs/viewEngine";
 import webRoutes from "./routes/web";
 
+const fs = require('fs');
+const https = require('https');
+
 require('dotenv').config();
+
+const httpsPort = 443;
+
+const httpsOptions = {
+    cert: fs.readFileSync('./ssl/wodaem_me.crt'),
+    ca: fs.readFileSync('./ssl/wodaem_me.ca-bundle'),
+    key: fs.readFileSync('./ssl/wodaem_me.key'),
+}
 
 let app = express();
 
@@ -11,11 +22,18 @@ viewEngine(app);
 
 webRoutes(app);
 
-app.use(express.json()); 
-app.use(express.urlencoded({
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
     extended: true
-})); 
+}));
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(">>> Log: App is running on port: " + process.env.SERVER_PORT);
-});
+app.use(function(request, response, next) {
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+    next();
+})
+
+const httpsServer = https.createServer(httpsOptions, app).listen(httpsPort, () => {
+    console.log("AA");
+})
