2b2aad940ed2ea7f1c046e720146a3499ae729a0
meadovv <hoangthanh2k3@gmail.com>
2023-08-12 20:44:41 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -4,27 +4,122 @@ let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 }
 
-let receive = (req, res) => {
-    return res.render('homepage.ejs');
-}
-
 let verify = (req, res) => {
-    // Parse the query params
+
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
 
-    // Check if a token and mode is in the query string of the request
     if (mode && token) {
-        // Check the mode and token sent is correct
         if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
-            // Respond with the challenge token from the request
             console.log("WEBHOOK_VERIFIED");
             res.status(200).send(challenge);
         } else {
-            // Respond with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
+    }
+}
+
+let receive = (req, res) => {
+    let body = req.body;
+
+    if (body.object === 'page') {
+
+        body.entry.forEach(function (entry) {
+
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+}
+
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+
+    if (received_message.text) {
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    let payload = received_postback.payload;
+
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    callSendAPI(sender_psid, response);
+}
+
+async function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    let url = new URL('https://graph.facebook.com/v17.0/me/messages');
+    url.search = new URLSearchParams({
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+    });
+    let res = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(request_body)
+    });
+    if (!res.ok) {
+        console.warn(
+            `Unable to call Send API: ${res.statusText}`,
+            await res.json()
+        );
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -4,63 +4,40 @@ import webRoutes from "./routes/web";
 
 let crypto = require("crypto")
 let { urlencoded, json } = require("body-parser")
-let fs = require('fs');
-let https = require('https');
-let http = require('http');
 
 require('dotenv').config();
 
-const ssl = {
-    cert: fs.readFileSync('./ssl/wodaem_me.crt'),
-    ca: fs.readFileSync('./ssl/wodaem_me.ca-bundle'),
-    key: fs.readFileSync('./ssl/wodaem_me.key'),
-}
-
-const app = express();
-app.enable('trust proxy')
-
-viewEngine(app);
-
-webRoutes(app);
-
+let app = express();
 app.use(json());
 app.use(urlencoded({
     extended: true
 }));
 
+viewEngine(app);
+
+webRoutes(app);
+
 // verify request from facebook
 app.use(json({ verify: verifyRequestSignature }));
 
-// auto redirect to https from http
-app.use((req, res, next) => {
-    if (req.protocol === 'http') {
-        return res.redirect("https://" + req.headers.host + req.url);
-    }
-    next();
-});
-
-http.createServer(app).listen(process.env.SERVER_PORT, () => {
-    console.log(">>> Log: Server is running on port " + process.env.SERVER_PORT + " in development.");
-})
-
-https.createServer(ssl, app).listen(process.env.SERVER_PORT_SSL, () => {
-    console.log(">>> Log: Server is running on port " + process.env.SERVER_PORT_SSL + " with ssl.");
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(">>> Log: Server is running on port " + process.env.SERVER_PORT);
 })
 
 function verifyRequestSignature(req, res, buf) {
     var signature = req.headers["x-hub-signature-256"];
-  
+
     if (!signature) {
-      console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+        console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
     } else {
-      var elements = signature.split("=");
-      var signatureHash = elements[1];
-      var expectedHash = crypto
-        .createHmac("sha256", config.appSecret)
-        .update(buf)
-        .digest("hex");
-      if (signatureHash != expectedHash) {
-        throw new Error("Couldn't validate the request signature.");
-      }
+        var elements = signature.split("=");
+        var signatureHash = elements[1];
+        var expectedHash = crypto
+            .createHmac("sha256", config.appSecret)
+            .update(buf)
+            .digest("hex");
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
     }
-  }
+}
